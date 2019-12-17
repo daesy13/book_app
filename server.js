5bd2c98048c4e36769a9364eabc441bcbb119570
daesy <daesyramos@hotmail.com>
2019-12-17 10:04:49 -0800

    test1 completed

--- server.js
+++ server.js
@@ -2,11 +2,20 @@
 
 // Dependencies
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const superagent = require('superagent');
 const PORT = process.env.PORT || 3000;
-app.use(cors());
+let ejs = require('ejs');
+app.set('view engine', 'ejs');
+
 require('dotenv').config();
+
+app.get('/hello', (req,res) => {
+  res.render('pages/index')
+}) 
+
+// ************************
+//   something goes here
+// ************************
 
 app.listen(3000, () => {console.log('3000 is connected');});
