bdea7e662d68d3a60d5b8ae8b6b85e7bdc8c4869
daesy <daesyramos@hotmail.com>
2019-12-17 10:32:03 -0800

    test3

--- server.js
+++ server.js
@@ -10,12 +10,21 @@ app.set('view engine', 'ejs');
 
 require('dotenv').config();
 
-app.get('/hello', (req,res) => {
+app.get('/', (req,res) => {
+
   res.render('pages/index')
 }) 
 
-// ************************
-//   something goes here
-// ************************
+app.post('/searches', (req, res) => {
+  superagent.get(`https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.author}`).then(data=> {
+
+    const books =  data.body.items.map(book => ({name: book.volumeInfo.title}));
+
+    console.log(books);
+
+    res.render('book-results', {books:books});
+  })
+})
+
 
 app.listen(3000, () => {console.log('3000 is connected');});
