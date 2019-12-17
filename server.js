1e16f12091d0aced4eaa28517366a5ea61048640
Crystal Soo Lee <soo.leesj@gmail.com>
2019-12-17 13:09:01 -0800

    radio button title and author

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const superagent = require('superagent');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
 require('dotenv').config();
@@ -16,14 +17,23 @@ app.get('/', (req,res) => {
 });
 
 app.post('/searches', (req, res) => {
-  superagent.get(`https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.author}`).then(data=> {
-
-    const books =  data.body.items.map(book => ({name: book.volumeInfo.title}));
-
-    console.log(books);
-
-    res.render('pages/searches/show', {books:books});
-  })
+  console.log('req.body :', req.body);
+  if (req.body.searchName === 'author') {
+    console.log('##############author!');
+    superagent.get(`https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.author}`).then(data=> {
+      const books =  data.body.items.map(book => ({name: book.volumeInfo.title}));
+      console.log(books);
+      res.render('pages/searches/show', {books:books});
+    })
+  }
+  if (req.body.searchName === 'title') {
+    console.log('##############title!');
+    superagent.get(`https://www.googleapis.com/books/v1/volumes?q=title+intitle:${req.body.title}`).then(data=> {
+      const books =  data.body.items.map(book => ({name: book.volumeInfo.title}));
+      console.log(books);
+      res.render('pages/searches/show', {books:books});
+    })
+  }
 })
 
 app.listen(PORT, ()=> {
